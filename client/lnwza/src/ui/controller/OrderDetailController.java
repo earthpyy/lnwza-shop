769fc3a00e9d679da03c242056cc072a40bf8b9f
pomprodpran <pomprodpran@hotmail.com>
2017-10-28 22:20:01 +0700

    add orderdetailcontroller
    -edit ui
    -create controller

---
+++ client/lnwza/src/ui/controller/OrderDetailController.java
@@ -0,0 +1,43 @@
+
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author pompr_000
+ */
+public class OrderDetailController {
+    @FXML
+    private TextField tf_orderno;
+
+    @FXML
+    private TextField tf_trackingno;
+
+    @FXML
+    private Button bt_update;
+
+    @FXML
+    private Button bt_cancle;
+
+    @FXML
+    private TableColumn<?, Integer> tb_id;
+
+    @FXML
+    private TableColumn<?, String> tb_name;
+
+    @FXML
+    private TableColumn<?, Integer> tb_qty;
+
+    @FXML
+    private TableColumn<?, Double> tb_price;
+
+    @FXML
+    private TableColumn<?, ?> tb_date;
+
+    @FXML
+    private TableColumn<?, ?> tb_status;
+}
