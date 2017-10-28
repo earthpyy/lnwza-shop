cc92b3dd14662a2261dd97d908a5037b263075d1
pomprodpran <pomprodpran@hotmail.com>
2017-10-28 22:01:59 +0700

    edit stockdetail
    -edit ui
    -create controller

---
+++ client/lnwza/src/ui/controller/StockDetailController.java
@@ -0,0 +1,37 @@
+
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author pompr_000
+ */
+public class StockDetailController {
+    @FXML
+    private ImageView im_product;
+
+    @FXML
+    private Label lb_id;
+
+    @FXML
+    private Label lb_name;
+
+    @FXML
+    private Label lb_price;
+
+    @FXML
+    private Label lb_size;
+
+    @FXML
+    private Label lb_color;
+
+    @FXML
+    private Label lb_descript;
+
+    @FXML
+    private Button bt_update;
+}
