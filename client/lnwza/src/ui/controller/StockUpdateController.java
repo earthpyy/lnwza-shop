3f8c21907cb03be218f27150892b4bd2756041cc
pomprodpran <pomprodpran@hotmail.com>
2017-10-28 22:12:20 +0700

    add stockupdatecontroller
    -edit some ui
    -add controller

---
+++ client/lnwza/src/ui/controller/StockUpdateController.java
@@ -0,0 +1,24 @@
+
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+/**
+ *
+ * @author pompr_000
+ */
+public class StockUpdateController {
+    @FXML
+    private ComboBox<?> cb_color;
+
+    @FXML
+    private TextField tf_instock;
+
+    @FXML
+    private TextField tf_change;
+
+    @FXML
+    private TextField tf_update;
+
+}
