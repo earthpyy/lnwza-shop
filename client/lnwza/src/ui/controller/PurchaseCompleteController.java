01f29f2c9fa635ab437fac7040d6811e9eb5d9cf
naremx <31928638+naremx@users.noreply.github.com>
2017-10-29 00:54:52 +0700

    Add PurchaseCompleteController

---
+++ client/lnwza/src/ui/controller/PurchaseCompleteController.java
@@ -0,0 +1,13 @@
+package ui.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+/**
+ *
+ * @author Naremx
+ */
+public class PurchaseCompleteController {
+    
+    @FXML
+    private Label lb_ordernum;
+    
+}
