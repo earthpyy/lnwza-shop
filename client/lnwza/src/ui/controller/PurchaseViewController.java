24d1cd2f12e507f76164ae922e098b83d195f67b
naremx <31928638+naremx@users.noreply.github.com>
2017-10-28 23:49:11 +0700

    Add PurchaseViewController

---
+++ client/lnwza/src/ui/controller/PurchaseViewController.java
@@ -0,0 +1,23 @@
+package ui.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+/**
+ *
+ * @author Naremx
+ */
+public class PurchaseViewController {
+    
+    @FXML
+    private ImageView im_product;
+
+    @FXML
+    private Label lb_name;
+
+    @FXML
+    private Label lb_price;
+
+    @FXML
+    private Button bt_add;
+}
