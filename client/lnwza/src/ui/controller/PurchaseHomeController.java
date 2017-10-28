ce15a82e0a9e74957a9923772d00a4c8d3f51b6e
naremx <31928638+naremx@users.noreply.github.com>
2017-10-28 21:53:20 +0700

    add PurchaseHomeController

---
+++ client/lnwza/src/ui/controller/PurchaseHomeController.java
@@ -0,0 +1,25 @@
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+/**
+ *
+ * @author SE-lnwza
+ */
+public class PurchaseHomeController {
+    
+    @FXML
+    private ImageView im_product;
+
+    @FXML
+    private Label lb_name;
+
+    @FXML
+    private Label lb_price;
+
+    @FXML
+    private Button bt_AddtoCart;
+    
+}
