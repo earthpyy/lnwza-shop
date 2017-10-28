e1460cdb7aea76f8d5666b66cef7fd4f1bd4e774
naremx <31928638+naremx@users.noreply.github.com>
2017-10-29 00:25:04 +0700

    Add PurchaseCheckoutController

---
+++ client/lnwza/src/ui/controller/PurchaseCheckoutController.java
@@ -0,0 +1,26 @@
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+/**
+ *
+ * @author Naremx
+ */
+public class PurchaseCheckoutController {
+    
+    @FXML
+    private TextField tf_qty;
+
+    @FXML
+    private TextField tf_total;
+
+    @FXML
+    private Button bt_cancel;
+
+    @FXML
+    private Button bt_credit;
+
+    @FXML
+    private Button bt_banking;
+}
