48dfa18ab9c9bb44d643a6e11c2ddfe6f0c055aa
Preeti Yuankrathok <preetisatit@gmail.com>
2017-10-28 23:15:49 +0700

    start transition to ObjectDB
    - add objectdb.jar
    - add some objectdb class
    - add dbport into appproperties

--- client/lnwza/src/application/DatabaseConnection.java
+++ client/lnwza/src/application/DatabaseConnection.java
@@ -1,28 +1,21 @@
 package application;
 
-import java.sql.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author SE-lnwza
  */
 public class DatabaseConnection {
-    private static String url = "jdbc:mysql://" + AppProperties.getDBIP() + ":3306/" + AppProperties.getDBName() + "?autoReconnect=true&useSSL=false";      
-    private static Connection con;
-
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(url, AppProperties.getDBUser(), AppProperties.getDBPass());
-//            System.out.println("Connection successful!");
-        } catch (Exception e) {
-            System.out.println("Failed to connect database!"); 
-            e.printStackTrace();
-        }
-        return con;
+    private static EntityManagerFactory emf;
+    
+    public static void load() {
+        emf = Persistence.createEntityManagerFactory("objectdb://" + AppProperties.getDBIP() + ":" + AppProperties.getDBPort() + "/" + AppProperties.getDBName() + ".odb;user=" + AppProperties.getDBUser() + ";password=" + AppProperties.getDBPass() + "");
+        System.out.println(emf.isOpen());
     }
     
-    public static boolean checkConnection() {
-        return (con != null);
+    public static EntityManagerFactory getConnection() {
+        return emf;
     }
 }
---
+++ client/lnwza/src/application/entity/Agent.java
@@ -0,0 +1,88 @@
+package application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author SE-lnwza
+ */
+@Entity
+public class Agent {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String tel;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Agent)) {
+            return false;
+        }
+        Agent other = (Agent) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "application.entity.Agent[ id=" + id + " ]";
+    }
+    
+}
