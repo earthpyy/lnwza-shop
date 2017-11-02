2540dd9eb99ec3ebad633ac86f8f5628def9be28
pomprodpran <pomprodpran@hotmail.com>
2017-11-02 20:29:05 +0700

    edit some code
    -order controller
    -order handler
    -order fxml

--- client/lnwza/src/ui/controller/OrderViewAgentController.java
+++ client/lnwza/src/ui/controller/OrderViewAgentController.java
@@ -1,8 +1,14 @@
 package ui.controller;
 
+import application.entity.Order;
+import application.handler.OrderHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 /**
  *
  * @author Naremx
@@ -11,14 +17,30 @@ public class OrderViewAgentController {
     
     @FXML
     private TextField tf_search;
+    
+    @FXML
+    private TableView<Order> tableView;
 
     @FXML
-    private TableColumn<?, Integer> tb_date;
+    private TableColumn<Order, Integer> tb_date;
 
     @FXML
-    private TableColumn<?, Integer> tb_ordernum;
+    private TableColumn<Order, Integer> tb_ordernum;
 
     @FXML
-    private TableColumn<?, String> tb_status;
+    private TableColumn<Order, String> tb_status;
+    
+    @FXML
+    protected void initialize() {
+        tb_date.setCellValueFactory(new PropertyValueFactory<>("orderDate"));
+        tb_ordernum.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tb_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        
+        
+        OrderHandler.load();
+        ObservableList<Order> data = FXCollections.observableArrayList(OrderHandler.getData());
+        tableView.setItems(data);
+    }
+    
     
 }
