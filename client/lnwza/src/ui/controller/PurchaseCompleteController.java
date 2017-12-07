6ab0d7b743ee9fcc95a9fa340c2edce9ccbacf29
naremx <31928638+naremx@users.noreply.github.com>
2017-12-07 19:10:03 +0700

    Add ImageButton PurchaseComplete

--- client/lnwza/src/ui/controller/PurchaseCompleteController.java
+++ client/lnwza/src/ui/controller/PurchaseCompleteController.java
@@ -1,10 +1,11 @@
 package ui.controller;
 import application.SceneLoader;
+import application.entity.ImageButton;
 import application.handler.OrderHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 /**
  *
  * @author SE-lnwza
@@ -13,17 +14,26 @@ public class PurchaseCompleteController {
 
     @FXML
     private Label lb_orderno;
-
+    
     @FXML
-    private Button bt_ok;
+    private AnchorPane anchorpane;
+    
+    //@FXML
+    //private Button bt_ok;
     
     @FXML
     protected void initialize() {
+        ImageButton bt_ok = new ImageButton("/ui/resources/images/button/ButtonOK.png");
+        bt_ok.setOnAction((event) -> {
+            ok();
+        });
+        anchorpane.getChildren().add(bt_ok);
+        
         lb_orderno.setText(OrderHandler.getLastId().toString());
     }
 
-    @FXML
-    void ok(ActionEvent event) {
+    
+    void ok() {
         SceneLoader.setPCBody("PurchaseHome");
     }
     
